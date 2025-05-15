09f74b31978254de930ed10f1d8bf2e0209e150d
Austin Tomas <Austin_Tomas@hotmail.com>
2025-05-15 15:40:41 -0600

    Started assignment and starting up weather_api.dart

---
+++ lib/services/weather_api.dart
@@ -0,0 +1,6 @@
+import 'dart:io';
+import 'package:http/http.dart' as http;
+
+const String weatherApiKey = '0f4f5f79bbfab25d0941b2aec6a701f2';
+const String currentWeatherEndpoint = 'https://api.openweathermap.org/data/2.5/weather';
+

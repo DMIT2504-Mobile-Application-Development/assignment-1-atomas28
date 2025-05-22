c047c21648cba1e4bf52875424b428a7192b0e02
Austin Tomas <Austin_Tomas@hotmail.com>
2025-05-22 10:54:38 -0600

    Gonna just commit some Exception handling

--- lib/models/current_weather.dart
+++ lib/models/current_weather.dart
@@ -27,6 +27,43 @@ class CurrentWeather{
 
 
 
+  //todo: GETTER AND SETTER + Exception for "description"
+  String get description{
+    return _description;
+  }
+
+  set description(String value) {
+    if(value.trim().isEmpty){
+      throw Exception('Description cannot be empty');
+    }
+    _description = value;
+  }
+
+  //todo: GETTER AND SETTER + Exception for "currentTemp" - must be between -100 and 100
+  double get currentTemp{
+    return _currentTemp;
+  }
+
+  set currentTemp(double value){
+    if(value > 100 || value < -100){
+      throw Exception('Temperature must be between -100 and 100');
+    }
+    _currentTemp = value;
+  }
+  
+  //todo: GETTER AND SETTER + Exception for "currentTime" (I'm assuming - github says "currentTemp") must ensure currentTime is not in the future
+  DateTime get currentTime{
+    return _currentTime;
+  }
+
+  set currentTime(DateTime value){
+    if(value > DateTime.now()){
+      throw Exception('Current time cannot be in the future');
+    }
+  }
+
+
+
 }
 
 
